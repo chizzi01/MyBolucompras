2d514345d0e9dd8c1d9883d058bde51a4e5d2b5a
Agustin Chizzini Melo <aguscn2001@gmail.com>
2025-01-08 12:10:12 -0300

    visualizacionn gastos timeline

--- mybolucompras/src/components/Timeline.jsx
+++ mybolucompras/src/components/Timeline.jsx
@@ -4,13 +4,15 @@ import '../App.css';
 const Timeline = ({ pagosFuturos }) => {
   const months = Object.keys(pagosFuturos);
   const payments = Object.values(pagosFuturos);
-
+  const formatNumber = (number) => {
+    return number.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  };
   return (
     <div className="timeline-container">
       {months.map((month, index) => (
         <div key={index} className="timeline-item">
           <div className="timeline-circle">{month}</div>
-          <div className="timeline-payment">${payments[index].toFixed(2)}</div>
+          <div className="timeline-payment">${formatNumber(payments[index])}</div>
         </div>
       ))}
     </div>
